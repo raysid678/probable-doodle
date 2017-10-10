73031ec277bcd57bc8fa1c2230f1ed84ee63ec89
Rehan <raysid678@gmail.com>
2017-10-10 23:27:19 +0000

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
+    http_basic_authenticate_with name: "dhh", password: "secret", except: :index
+
+  before_action :set_micropost, only: [:show, :edit, :update, :destroy]
+
+  # GET /microposts
+  # GET /microposts.json
+  def index
+    @microposts = Micropost.all
+  end
+  
   protect_from_forgery with: :exception
+  
   
   def hello
       render html: "hello,world!"
   end
+  
 end
